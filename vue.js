9f1ebc5bf028987205138a4b6be79eefc593b2a3
dcln00 <nii.aryh@gmail.com>
2022-06-01 01:17:11 +0000

    new project added

--- vue.js
+++ vue.js
@@ -2,42 +2,48 @@ const app = Vue.createApp({
     data() {
         return {
             projects: [
+                {framework: 'Nuxt JS',
+                frameworkColor: 'new-btn nuxt',
+                url: '#',
+                img: 'thumbs/thumbnail-08.jpg',
+                title: '01/ Styled By Selly ecommerce store (coming soon)'
+                },
                 {framework: 'Vue JS',
                 frameworkColor: 'new-btn vue',
                 url: 'https://themeaduane.vercel.app/',
                 img: 'thumbs/thumbnail-06.png',
-                title: '01/ theme aduane'
+                title: '02/ theme aduane'
                 },
                 {framework: 'Bootstrap',
                 frameworkColor: 'new-btn bootstrap',
                 url: 'https://projects.niiaryeh.me/persona-website/',
                 img: 'thumbs/thumbnails-05.png',
-                title: '02/ getpersona.app online store'
+                title: '03/ getpersona.app online store'
                 },
                 {framework: 'Bootstrap',
                 frameworkColor: 'new-btn bootstrap',
                 url: 'https://projects.niiaryeh.me/tripzzafrica/',
                 img: 'thumbs/thumbnails-04.png',
-                title: '03/ tripzzafrica'
+                title: '04/ tripzzafrica'
                 },
                 {framework: 'Bootstrap',
                 frameworkColor: 'new-btn bootstrap',
                 url: 'https://projects.niiaryeh.me/61x3s',
                 img: 'thumbs/thumbnails-07.png',
-                title: '04/ Theme 61x3s'
+                title: '05/ Theme 61x3s'
                 },
                 {framework: 'html / css',
                 frameworkColor: 'new-btn html',
                 url: 'https://projects.niiaryeh.me/wavebox-realty-project/',
                 img: 'thumbs/thumbnails-03.png',
-                title: '05/ Wavebox Properties Landing Page'
+                title: '06/ Wavebox Properties Landing Page'
                 },
-                {framework: 'html / css',
-                frameworkColor: 'new-btn html',
-                url: 'https://projects.niiaryeh.me/Monochrome-Profile-Web/',
-                img: 'thumbs/thumbnails-02.png',
-                title: '06/ Monochrome Profile Website'
-                },
+                // {framework: 'html / css',
+                // frameworkColor: 'new-btn html',
+                // url: 'https://projects.niiaryeh.me/Monochrome-Profile-Web/',
+                // img: 'thumbs/thumbnails-02.png',
+                // title: '06/ Monochrome Profile Website'
+                // },
 
             ]    
         }
