f23e3f08b250db633bc1ee4f52b67118aacaecc5
dcln00 <nii.aryh@gmail.com>
2022-11-27 15:24:23 +0000

    updates

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-let highlightBtn = document.querySelector('.highlight-btn');
-let codeBlock = document.getElementById('codediv');
+let themeBtn = document.querySelector('.theme');
 
 
-    highlightBtn.addEventListener('click', function() {
-
-        window.getSelection().selectAllChildren(
-            codeBlock
-        );
-
-        console.log('Code selected');
-    })
-
-
+	function getMode() {
+		let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+		localStorage.getItem('projects.theme') ? theme = localStorage.getItem('projects.theme') : null
+		return theme
+	}
+	
+	function loadTheme(theme) {
+		const root = document.querySelector(':root')
+		if(theme ==='dark') {
+			themeBtn.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>'
+		} else {
+			themeBtn.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>'
+		}
+		root.setAttribute('color-scheme', `${theme}`)
+	}
+	
+	themeBtn.addEventListener('click', function() {
+		let theme = getMode()
+		if(theme === 'dark') {
+			theme = 'light'
+		} else {
+			theme = 'dark'
+		}
+		localStorage.setItem('projects.theme', `${theme}`)
+		loadTheme(theme)
+	})
+	
+	window.addEventListener('DOMContentLoaded', function() {
+		loadTheme(getMode())
+	})
