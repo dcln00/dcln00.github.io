767e952d6201403bf3d1087d38d07382ca9198e4
dcln00 <nii.aryh@gmail.com>
2023-07-05 04:25:54 +0000

    changes

--- assets/scripts/vue.js
+++ assets/scripts/vue.js
@@ -5,52 +5,21 @@ const app = Vue.createApp({
                 {framework: 'Nuxt JS',
                 frameworkColor: 'new-btn nuxt',
                 url: '#',
-                img: '/assets/images/thumbs/thumbnail-08.jpg',
-                title: '01/ Styled By Selly ecommerce store (coming soon)'
-                },
-				{framework: 'Nuxt JS x Sanity',
-                frameworkColor: 'new-btn nuxt',
-                url: 'https://nanapoley.com/',
-                img: '/assets/images/thumbs/thumbnail-10.png',
-                title: '02/ Nana Poley'
+                img: '/assets/images/thumbs/streamlab.png',
+                title: '01/ Streamlab'
                 },
 				{framework: 'Nuxt JS',
                 frameworkColor: 'new-btn nuxt',
                 url: 'https://eisenlegal.vercel.app/',
                 img: '/assets/images/thumbs/thumbnail-09.jpg',
-                title: '03/ eisenlegal'
+                title: '02/ eisenlegal'
                 },
                 {framework: 'Vue JS',
                 frameworkColor: 'new-btn vue',
                 url: 'https://themeaduane.vercel.app/',
                 img: '/assets/images/thumbs/thumbnail-06.png',
-                title: '04/ theme aduane'
+                title: '03/ theme aduane'
                 },
-                {framework: 'Bootstrap',
-                frameworkColor: 'new-btn bootstrap',
-                url: 'https://projects.niiaryeh.me/tripzzafrica/',
-                img: '/assets/images/thumbs/thumbnails-04.png',
-                title: '05/ tripzzafrica'
-                },
-                {framework: 'Bootstrap',
-                frameworkColor: 'new-btn bootstrap',
-                url: 'https://projects.niiaryeh.me/61x3s',
-                img: '/assets/images/thumbs/thumbnails-07.png',
-                title: '06/ Theme 61x3s'
-                },
-                {framework: 'html / css',
-                frameworkColor: 'new-btn html',
-                url: 'https://projects.niiaryeh.me/wavebox-realty-project/',
-                img: '/assets/images/thumbs/thumbnails-03.png',
-                title: '07/ Wavebox Properties Landing Page'
-                },
-                // {framework: 'html / css',
-                // frameworkColor: 'new-btn html',
-                // url: 'https://projects.niiaryeh.me/Monochrome-Profile-Web/',
-                // img: 'thumbs/thumbnails-02.png',
-                // title: '06/ Monochrome Profile Website'
-                // },
-
             ]    
         }
     },
